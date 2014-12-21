e6fb0a3e5f1663b2921984143838e78ae3ea2bd8
Wang Song <wangsong@lpld.com>
2014-12-21 21:05:51 +0800

    implement: add system_MK60F15.h

--- lib/CPU/system_MK60F15.h
+++ lib/CPU/system_MK60F15.h
@@ -1,8 +1,8 @@
 /**
- * @file system_MK60F12.h
+ * @file system_MK60F15.h
  * @version 1.2.1[By LPLD]
  * @date 2013-06-18
- * @brief MK60F12系列单片机系统配置文件
+ * @brief MK60F15系列单片机系统配置文件
  *
  * 更改建议:禁止修改
  *
