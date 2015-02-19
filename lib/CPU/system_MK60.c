379006078de0b2c6b1809f3455b01fd19ddce21a
Wang Song <wangsong@lpld.cn>
2015-02-19 23:06:12 +0800

    implement: add f and D's reset diagnostic

--- lib/CPU/system_MK60.c
+++ lib/CPU/system_MK60.c
@@ -403,6 +403,47 @@ void Diagnostic_Reset_Source(void)
           printf("Low-voltage Detect Reset\n");
   if (MC->SRSL & MC_SRSL_WAKEUP_MASK)
           printf("LLWU Reset\n");
+#elif (defined(CPU_MK60F12) || defined(CPU_MK60F15) || defined(CPU_MK60D10))
+  
+  if (RCM->SRS1 & RCM_SRS1_SACKERR_MASK)
+          printf("Stop Mode Acknowledge Error Reset\n");
+  if (RCM->SRS1 & RCM_SRS1_EZPT_MASK)
+          printf("EzPort Reset\n");
+  if (RCM->SRS1 & RCM_SRS1_MDM_AP_MASK)
+          printf("MDM-AP Reset\n");
+  if (RCM->SRS1 & RCM_SRS1_SW_MASK)
+          printf("Software Reset\n");
+  if (RCM->SRS1 & RCM_SRS1_LOCKUP_MASK)
+          printf("Core Lockup Event Reset\n");
+  if (RCM->SRS1 & RCM_SRS1_JTAG_MASK)
+          printf("JTAG Reset\n");
+  if (RCM->SRS0 & RCM_SRS0_POR_MASK)
+          printf("Power-on Reset\n");
+  if (RCM->SRS0 & RCM_SRS0_PIN_MASK)
+          printf("External Pin Reset\n");
+  if (RCM->SRS0 & RCM_SRS0_WDOG_MASK)
+          printf("Watchdog(COP) Reset\n");
+  if (RCM->SRS0 & RCM_SRS0_LOC_MASK)
+          printf("Loss of Clock Reset\n");
+  if (RCM->SRS0 & RCM_SRS0_LVD_MASK)
+          printf("Low-voltage Detect Reset\n");
+  if (RCM->SRS0 & RCM_SRS0_WAKEUP_MASK)
+  {
+    printf("[outSRS]Wakeup bit set from low power mode exit\n");
+    printf("[outSRS]SMC_PMPROT = %#02X ", (SMC->PMPROT))  ;
+    printf("[outSRS]SMC_PMCTRL = %#02X ", (SMC->PMCTRL))  ;
+    printf("[outSRS]SMC_VLLSCTRL = %#02X ", (SMC->VLLSCTRL))  ;
+    printf("[outSRS]SMC_PMSTAT = %#02X \r\n", (SMC->PMSTAT))  ;
+
+    if ((SMC->PMCTRL & SMC_PMCTRL_STOPM_MASK)== 3)
+      printf("[outSRS] LLS exit \n") ;
+    if (((SMC->PMCTRL & SMC_PMCTRL_STOPM_MASK)== 4) && ((SMC->VLLSCTRL & SMC_VLLSCTRL_VLLSM_MASK)== 1))
+      printf("[outSRS] VLLS1 exit \n") ;
+    if (((SMC->PMCTRL & SMC_PMCTRL_STOPM_MASK)== 4) && ((SMC->VLLSCTRL & SMC_VLLSCTRL_VLLSM_MASK)== 2))
+      printf("[outSRS] VLLS2 exit \n") ;
+    if (((SMC->PMCTRL & SMC_PMCTRL_STOPM_MASK)== 4) && ((SMC->VLLSCTRL & SMC_VLLSCTRL_VLLSM_MASK)== 3))
+      printf("[outSRS] VLLS3 exit \n") ; 
+  }
 #endif
 #endif
 }
